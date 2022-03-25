e4700dc41c5006940e09d94b8f670e0c1651654d
laron98 <72889951+laron98@users.noreply.github.com>
2022-03-25 17:33:37 -0400

    Add files via upload

---
+++ ready.c
@@ -0,0 +1,40 @@
+/* ready.c - ready */
+
+#include <xinu.h>
+
+struct queue *readyqueue;	/** ready queue */
+
+
+/**
+ * Put process on ready queue
+ * @param pid	ID of process to place on ready queue
+ * @param resch	reschedule afterward?
+ */
+status	ready(pid32 pid, bool8 resch)
+{
+	register struct procent *prptr;
+
+	if (isbadpid(pid))
+		return(SYSERR);
+
+	// set process state to indicate ready and add to ready list
+	prptr = &proctab[pid];
+
+	// TODO - set the process' state pointed by prptr to "ready"
+	prptr->prstate = PR_READY;	//DC REMOVE
+
+	// TODO - enqueue the process
+	if (AGING != FALSE){ 
+		struct qentry *temp = readyqueue->head;
+		while(temp != NULL){
+			temp->key++;
+			temp = temp->next;
+		}
+	}
+	enqueue(pid, readyqueue, prptr->prprio); //DC REMOVE
+
+	if (resch == RESCHED_YES)
+		resched();
+
+	return OK;
+}
